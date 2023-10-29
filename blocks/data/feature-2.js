2f513f65b9962d68575cbb127d1dde416bbf6600
Hugo Carvalho <hugo.deleon6243@gmail.com>
2023-10-29 04:57:34 -0300

    ✨ Traduz feature-2

--- blocks/data/feature-2.js
+++ blocks/data/feature-2.js
@@ -1,32 +1,32 @@
 export const source = `
 <section class="text-gray-600 body-font">
-   <div class="container px-5 py-24 mx-auto">
-      <div class="text-center mb-20">
-         <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Raw Denim Heirloom Man Braid</h1>
-         <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug.</p>
-         <div class="flex mt-6 justify-center">
+    <div class="container px-5 py-24 mx-auto">
+        <div class="text-center mb-20">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Nossos serviços</h1>
+            <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Conheça as características da empresa que irá te oferecer o melhor do mercado.</p>
+            <div class="flex mt-6 justify-center">
             <div class="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
-         </div>
-      </div>
-      <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
-         <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
-               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
-                  <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-               </svg>
             </div>
+        </div>
+        <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6 justify-center">
+            <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+                <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                </svg>
+                </div>
             <div class="flex-grow">
-               <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Shooting Stars</h2>
-               <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+               <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Agilidade</h2>
+               <p class="leading-relaxed text-base">Oferecemos um serviço que valoriza e preserva o seu precioso tempo.</p>
                <a class="mt-3 text-indigo-500 inline-flex items-center">
-                  Learn More
+                  Saiba Mais
                   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                      <path d="M5 12h14M12 5l7 7-7 7"></path>
                   </svg>
                </a>
             </div>
-         </div>
-         <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+        </div>
+        <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
             <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
                   <circle cx="6" cy="6" r="3"></circle>
@@ -35,17 +35,17 @@ export const source = `
                </svg>
             </div>
             <div class="flex-grow">
-               <h2 class="text-gray-900 text-lg title-font font-medium mb-3">The Catalyzer</h2>
-               <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+               <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Transparência</h2>
+               <p class="leading-relaxed text-base">Todas as modificações realizadas passam por um processo de pré-aprovação conduzida pela cliente.</p>
                <a class="mt-3 text-indigo-500 inline-flex items-center">
-                  Learn More
+                  Saiba Mais
                   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                      <path d="M5 12h14M12 5l7 7-7 7"></path>
                   </svg>
                </a>
             </div>
-         </div>
-         <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+        </div>
+        <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
             <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
                   <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
@@ -53,19 +53,17 @@ export const source = `
                </svg>
             </div>
             <div class="flex-grow">
-               <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Neptune</h2>
-               <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+               <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Qualidade</h2>
+               <p class="leading-relaxed text-base">Todos os produtos oferecidos são testados e aprovados por clientes no mundo todo.</p>
                <a class="mt-3 text-indigo-500 inline-flex items-center">
-                  Learn More
+                  Saiba Mais
                   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                      <path d="M5 12h14M12 5l7 7-7 7"></path>
                   </svg>
                </a>
             </div>
-         </div>
-      </div>
-      <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-   </div>
+        </div>
+    </div>
 </section>
 
 `;
